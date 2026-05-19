b18f50941fb34070902c0ddabeeb45de2108e447
Sofia Rojas <sofiarojasuptm@gmail.com>
2026-05-19 13:48:33 -0400

    Usando React.Children y React.Clone

--- src/TodoComponents/TodoHeader.js
+++ src/TodoComponents/TodoHeader.js
@@ -1,12 +1,14 @@
 import React from "react"
 
+// Esta es una de las maneras de pasar propiedades desde un padre (header) a los hijos (counter y search)
+
 function TodoHeader ({children, loading}) {
     return(
         <header>
             {
-                React.Children
-                .toArray(children)
-                .map(child => React.cloneElement(child, {loading}))
+                React.Children // sirve para recorrer los hijos dentro de un componente padre
+                .toArray(children) // Convierte los hijos en array
+                .map(child => React.cloneElement(child, {loading})) // map recorre, se clonan a los hijos y se les pasa la propiedad loading
                 }
         </header>
     )
